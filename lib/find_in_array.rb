908d173ae082178e2dff8c7647f5833095a1476c
Kai Peng Li <oopanpan@gmail.com>
2020-12-16 08:39:44 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,10 +1,9 @@
 
 def find_element_index(array, value_to_find)
-  final_value = nil
+  #final_value = nil
   array.length.times {|i|
     if array[i] == value_to_find
-      final_value = i
+      return i
     end
   }
-  final_value
 end
