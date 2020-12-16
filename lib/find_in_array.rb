bce4a29213fa6078e27185174c797881b504cc37
Kai Peng Li <oopanpan@gmail.com>
2020-12-16 08:24:09 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,14 +1,15 @@
-require 'pry'
+#require 'pry'
 
 def find_element_index(array, value_to_find)
   array.length.times {|i|
     if array[i] == value_to_find
       return i
+    else
+      return nil
     end
-  binding.pry
   }
 end
 
 
-sasha = [0,1,2,3,4,5,6,7]
-find_element_index(sasha, 4)
+#sasha = [0,1,2,3,4,5,6,7]
+#find_element_index(sasha, 4)
