311af1ae939fc7b0174888a0384c18ccfbec468e
Kai Peng Li <oopanpan@gmail.com>
2020-12-16 08:19:33 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,5 +1,11 @@
+require 'pry'
+
 def find_element_index(array, value_to_find)
   array.length.times {|i|
-  
+    array[i] == value_to_find
+    
 end
 
+
+sasha = [0,1,2,3,4,5,6,7]
+find_element_index(sasha, 4)
